c00f21b7fb45d6cbe6d5af7f5423c4ecf7beb386
Danylo <danylo@code-care.pro>
2022-12-20 12:16:16 +0200

    Remove bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
-// Importing the Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>

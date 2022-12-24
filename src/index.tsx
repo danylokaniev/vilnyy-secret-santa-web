e032128079323a89b45dcae4a65bc486e0ab1bd9
Danylo <danylo@code-care.pro>
2022-12-24 17:11:50 +0200

    Add version console

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+console.log('Vilnyy Charity Project v1.0.1');
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>

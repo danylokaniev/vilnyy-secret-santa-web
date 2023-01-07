2ce1b61689c5a4c22f61f4d790f3dbbf5e9e902c
Danylo <danylo@code-care.pro>
2023-01-07 21:45:02 +0200

    Display version from package json

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App/App';
+import packageJson from '../package.json';
+import './index.css';
 
-console.log('Vilnyy Charity Project v1.0.1');
+console.log(`Vilnyy Charity Project v${packageJson.version}`);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(

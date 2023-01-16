04581eb25761732ee4c5ab123971b5289b0f76b4
Danylo <danylo@code-care.pro>
2023-01-16 11:39:17 +0200

    Update styles for Title & Description

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,8 +3,11 @@ import style from './Title.module.css';
 const Title = () => {
   return (
     <div className={style.title}>
-      <p>Збираємо на батареї для</p>
-      <p> квадрокоптерів Mavic 3</p>
+      <p>
+        Збираємо на батареї для
+        <br className={style.desktopBreak} /> квадрокоптерів <br className={style.mobileBreak} />
+        Mavic 3
+      </p>
     </div>
   );
 };
